3549ba4d7a43f16c902ae624f6d3b3572138966b
yashHindgreco <162083668+yashHindgreco@users.noreply.github.com>
2025-06-28 22:30:34 +0530

    log update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -21,16 +21,21 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
   try{
 
+    console.log(req.body)
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+  console.log(user)
   if (!user) return res.status(401).json({ msg: 'Invalid credentials' });
 
   const isMatch = await bcrypt.compare(password, user.hashedPassword);
+  console.log(isMatch)
   if (!isMatch) return res.status(401).json({ msg: 'Invalid credentials' });
 
   const token = jwt.sign({ userId: user._id, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  console.log(token)
   res.json({ token });
   }catch(err){
+    console.log(err.message)
     res.status(500).json({error:err.message})
   }
 });
