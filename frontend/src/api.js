5f2f967ff7dbcd6677b0babccc4adb3fc9ebe943
yashHindgreco <162083668+yashHindgreco@users.noreply.github.com>
2025-06-28 22:08:57 +0530

    axios url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL:'https://password-vault-rho.vercel.app/api'
 });
 
 // Add JWT token to headers
